d655e2997a6f2f3f128e461fbd915b75ddcf2c6f
rubin110 <rubin@starset.net>
2011-05-30 22:20:37 -0700

    more stuff

---
+++ _cid/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+   "http://www.w3.org/TR/html4/strict.dtd">
+<?php
+
+	
+?>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>index</title>
+</head>
+<body>
+<h2>Journey Log</h2>
+<p>Designation: <?php echo $_COOKIE["jlog-cid"]; ?>
+<p><a href="setcid.php">Set your designation</a>
+</body>
+</html>
---
+++ _cid/setcid.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+   "http://www.w3.org/TR/html4/strict.dtd">
+<?php
+$cid = $_GET['cid']; // Get the cid
+$possiblecid = array("CP0", "CP1A", "CP1B", "CP2A", "CP2B", "CP3", "CP4", "CP5", "CP6", "CP7", "BA", "BB", "X"); // List of valid cids
+if (in_array($cid, $possiblecid)) // Is it valid? If so go.
+	{
+		setcookie("jlog-cid", $cid, time()+86400); // Set cookies
+	}
+	
+?>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>index</title>
+</head>
+<body>
+<a href="/_cog">Back</a><br><br>
+<?php
+if (isset($cid)) // Is cid populated?
+	{
+		echo "<p><b><big>Your designation is now set to: $cid</b></big><br><br>"; // State new cid
+	};
+	?>
+Set your designation:<br>
+<a href="setcid.php?cid=CP0">CP0</a><br>
+<a href="setcid.php?cid=CP1A">CP1A</a><br>
+<a href="setcid.php?cid=CP1B">CP1B</a><br>
+<a href="setcid.php?cid=CP2A">CP2A</a><br>
+<a href="setcid.php?cid=CP2B">CP2B</a><br>
+<a href="setcid.php?cid=CP3">CP3</a><br>
+<a href="setcid.php?cid=CP4">CP4</a><br>
+<a href="setcid.php?cid=CP5">CP5</a><br>
+<a href="setcid.php?cid=CP6">CP6</a><br>
+<a href="setcid.php?cid=CP7">CP7</a><br>
+<a href="setcid.php?cid=BA">BA</a><br>
+<a href="setcid.php?cid=BB">BB</a><br>
+<a href="setcid.php?cid=X">X</a><br>
+
+</body>
+</html>
