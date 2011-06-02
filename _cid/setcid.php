8e54f38187c2492b22bd19358d186aee8f06cfbc
Ed <none@none>
2011-06-02 00:26:51 -0700

    Cleaning out old code we don't need.

--- _cid/setcid.php
+++ _cid/setcid.php
@@ -24,49 +24,4 @@ if ($cid == 9999) {
 }
 
 
-
-die();
-
-$possiblecid = array("CP0", "CP1A", "CP1B", "CP2A", "CP2B", "CP3", "CP4", "CP5", "CP6", "CP7", "BA", "BB"); // List of valid cids
-if (in_array($cid, $possiblecid)) // Is it valid? If so go.
-	{
-		setcookie("jlog-cid", $cid, time()+86400, "/", $_SERVER['SERVER_NAME']); // Set cookies
-	}
-	elseif ($cid == "X") // If $cid is X, then kill the cookie
-	{
-		setcookie("jlog-cid", $cid, time()-86400, "/", $_SERVER['SERVER_NAME']);
-	}
 ?>
-<html lang="en">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<title>index</title>
-</head>
-<body>
-<a href="/_cog">Back</a><br><br>
-<?php
-if (in_array($cid, $possiblecid)) // Is cid populated?
-	{
-		echo "<p><b><big>Your designation is now set to: $cid</b></big><br><br>"; // State new cid
-	}
-else {
-	echo "<p><b><big>Your designation isn't set</b></big><br><br>"; // State lack of designation
-}
-	?>
-Set your designation:<br>
-<a href="setcid.php?cid=CP0">CP0</a><br>
-<a href="setcid.php?cid=CP1A">CP1A</a><br>
-<a href="setcid.php?cid=CP1B">CP1B</a><br>
-<a href="setcid.php?cid=CP2A">CP2A</a><br>
-<a href="setcid.php?cid=CP2B">CP2B</a><br>
-<a href="setcid.php?cid=CP3">CP3</a><br>
-<a href="setcid.php?cid=CP4">CP4</a><br>
-<a href="setcid.php?cid=CP5">CP5</a><br>
-<a href="setcid.php?cid=CP6">CP6</a><br>
-<a href="setcid.php?cid=CP7">CP7</a><br>
-<a href="setcid.php?cid=BA">BA</a><br>
-<a href="setcid.php?cid=BB">BB</a><br>
-<a href="setcid.php?cid=X">X</a><br>
-
-</body>
-</html>
--- index.php
+++ index.php
@@ -28,20 +28,4 @@ if (is_valid_runner($jlogRID)) {
 	print "Go to the runner bind page";
 }
 
-die();	//Don't execute below here
-
-
-if (($_COOKIE["jlog-cid"]) == "CP0") { header( "Location: /_reg/?rid=$rid" ); } // If the cid cookie for registration is set, go to registration page
-elseif (isset($_COOKIE["jlog-cid"])) { header( "Location: /_cp/?cid=$cid&rid=$rid" ); } // If the cid cookie is set for all other checkpoints, go to the checkpoint page
-elseif (isset($_COOKIE["jlog-rid"])) { header( "Location: /_tag/?rid=$rid" ); } // If the rid cookie is set, go to the tag page
-else { header( "Location: /_rid/?rid=$rid" ); } // If the rid cookie isn't set, go to the bind page
 ?>
-<html lang="en">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<title>Journey Log</title>
-</head>
-<body>
-<?php echo $rid ?>
-</body>
-</html>
