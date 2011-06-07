2a44bb08a671f952f9adcc16e6bb93b771d053da
Ed <none@none>
2011-06-06 22:17:31 -0700

    Registration via staff

---
+++ www/manualregister.php
@@ -0,0 +1,8 @@
+<? //This is just a sample file useful for testing _autoregistration.php. I guess we could turn it into an actual registration if we need it ?>
+
+<form action="_autoregistration.php" method="POST" enctype="multipart/form-data">
+Runner ID: <input name="runner_id" type="text"><br />
+Photo: <input name="player_photo" type="file"><br />
+<input type="submit" value="Register">
+
+</form>
