1a715bcffb6d655277b401e719aea436ca65bcaa
rubin110 <rubin@starset.net>
2011-06-08 04:20:09 -0700

    More CSS and wording and shit.

--- www/_tag.php
+++ www/_tag.php
@@ -2,7 +2,7 @@
 //user has a jlog-rid cookie set, so this means they scanned the qr code of someone they tagged
 include('mobile-friendly.html');
 include('functions.php');
-
+print '<h2>Journey Log</h2>';
 $tagger_id = clean_chaser_id($_GET['tid']);
 $runner_id = clean_runner_id($_GET['rid']);
 
@@ -11,27 +11,27 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 	$runner_id = clean_chaser_id($_POST['runner_id']);
 	$loc_lat = $_POST['latitude'];
 	$loc_long = $_POST['longitude'];
+	$loc_addr = $_POST['address'];
 	if (empty($loc_lat)) {
 		$loc_lat = 0;
 	}
 	if (empty($loc_long)) {
 		$loc_long = 0;
 	}
-	print "Registering tag: Tagger ".$tagger_id." tagged Runner ".$runner_id." at ".$loc_lat.",".$loc_long."<br />";
+	print '<h3>Registering Tag</h3>
+	<p><span style="color:red;">Chaser '.$tagger_id.'</span> tagged <span style="color:blue;">Runner '.$runner_id.'</span> at '.$loc_lat.','.$loc_long.'.<br />';
 	if (is_valid_runner($tagger_id) && is_valid_runner($runner_id)) {
 		if (register_tag($tagger_id, $runner_id, $loc_lat, $loc_long)) {
-			print "Oh shit, you tagged someone! Good job<br />";
-			print "Tag registered!<br />";
+			print '<h3>Your tag is checked in!</h3><div style="font-size:14em;color:green;text-align: center;">&#10003;</div>';
 		} else {
 			if (tag_exists($tagger_id, $runner_id)) {
-				print "You've already tagged this runner.<br />";
+				print '<h3>You\'ve already tagged this runner!</h3><div style="font-size:14em;color:red;text-align: center;">X</div>';
 			} else {
-				print "Oops, something is broken<br />";
+				print '<h3>Oops, something is broken</h3><div style="font-size:14em;color:red;text-align: center;">X</div>';
 			}
 		}
 	} else {
-		print "Invalid data<br />";
-		if (DEBUG) print "Invalid tagger or runner<br />";
+		if (DEBUG) print '<h3>Invalid tagger or runner</h3><div style="font-size:14em;color:red;text-align: center;">X</div>';
 	}
 
 } else {
@@ -52,16 +52,22 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 	  alert('Could not find location');
 	}
 </script>
-
+<h3>Checkin your Tag</h3>
+<p>Note: Be sure the <span style="color:red;">Chaser</span> is scanning the <span style="color:blue;">Runner's</span> Runner ID on the <span style="color:red;">Chaser's</span> phone!
 <form name="tag_runner" action="<?= $_SERVER['PHP_SELF']; ?>" method="post">
 <?php if (is_valid_runner($tagger_id)){
-print '<p><span style="color=red;">Pursuing Chaser: </span><br><input id="tagger_id" name="tagger_id" value="'.$tagger_id.'" readonly>';
+	print '<p><div style="color:red;">Pursuing Chaser: </div><input id="tagger_id" name="tagger_id" value="'.$tagger_id.'" readonly>';
 }
 else {
-	print '<p><span style="color=red;">Pursuing Chaser: </span><br><input id="tagger_id" name="tagger_id" value="">';
+	print '<p><div style="color:red;">Pursuing Chaser: </div><input id="tagger_id" name="tagger_id" value="">';
+};
+if (is_valid_runner($runner_id)){
+	print '<p><div style="color:blue;">Tagged Runner: </div><input id="runner_id" name="runner_id" value="'.$runner_id.'" readonly>';
+}
+else {
+	print '<p><div style="color:blue;">Tagged Runner: </div><input id="runner_id" name="runner_id" value="">';
 };
 ?>
-<p><span style="color=blue;">Tagged Runner: </span><br><input id="runner_id" name="runner_id" value="<?= $runner_id ?>">
 <p>Lat:<br><input id="latitude" type="text" name="latitude" value="" readonly>
 <p>Log:<br><input id="longitude" type="text" name="longitude" value="" readonly>
 <p><input type="submit" value="I tagged them!"></p>
