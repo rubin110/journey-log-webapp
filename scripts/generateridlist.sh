71a39a03a3c2a32c127eedce16defc02aa50cd40
rubin110 <rubin@starset.net>
2011-06-14 20:18:34 -0700

    FUCKING DONE!

--- scripts/generateridlist.sh
+++ scripts/generateridlist.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
-COUNT="$1"
+COUNT=$1
 echo "Making sure the runneridtable exists"
 touch runneridtable
 CURRENTNUMBER=0
 echo "Start generation"
-while  [ $COUNT -le $CURRENTNUMBER ]
+while  [ "$CURRENTNUMBER" -lt "$COUNT"  ]
 do
-NEWID=`</dev/urandom tr -dc A-NP-Z1-9 | head -c5`
-CHECKID=`/bin/grep $NEWID runneridtable`
+NEWID=`</dev/urandom tr -dc A-NP-Z1-9 | head -c5`	# Linux
+# NEWID=`echo $RANDOM`								# Mac
+CHECKID=`/usr/bin/grep $NEWID runneridtable` 		# Mac
+# CHECKID=`/bin/grep $NEWID runneridtable` 			# Linux
 if [ "$CHECKID" = "" ]
 	then
 	echo $NEWID >> runneridtable
 else
-echo "Found duplicate"
+echo "Found duplicate, killed"
 fi
-CURRENTNUMBER=`cat runnertable | wc -l`
+CURRENTNUMBER=`cat runneridtable | wc -l`
 done
 echo "Generation complete"
 echo "Double checking for duplicates"
