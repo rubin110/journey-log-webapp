3d73184badf84933db51cc49507ef44e4255f685
Thomas Lotze <thomas.lotze@thomaslotze.com>
2012-03-06 00:44:18 -0800

    remember to commit updated sql

--- scripts/simpler_austin_schema.sql
+++ scripts/simpler_austin_schema.sql
@@ -6,12 +6,11 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 DROP TABLE IF EXISTS `checkins`;
 CREATE TABLE `checkins` (
-  `checkin_id` int(11) NOT NULL AUTO_INCREMENT,
-  `runner_id` int(11),
-  `checkpoint_id` int(11),
-  `checkin_time` datetime DEFAULT NULL,
-  PRIMARY KEY (`checkin_id`),
-  UNIQUE KEY `runner_id` (`runner_id`,`checkpoint_id`),
+  `runner_id` char(5) COLLATE utf8_unicode_ci NOT NULL,
+  `checkpoint_id` char(5) COLLATE utf8_unicode_ci NOT NULL,
+  `checked_in_earlier` int(11) NOT NULL DEFAULT 0,
+  `checkin_time` datetime NOT NULL,
+  PRIMARY KEY (`runner_id`,`checkpoint_id`),
   KEY `index_checkins_on_checkpoint_id_and_time` (`checkpoint_id`,`checkin_time`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ;
 
@@ -23,20 +22,26 @@ CREATE TABLE `checkins` (
 
 DROP TABLE IF EXISTS `checkpoints`;
 CREATE TABLE `checkpoints` (
-  `checkpoint_id` int(11) NOT NULL AUTO_INCREMENT,
+  `checkpoint_id` char(5) COLLATE utf8_unicode_ci NOT NULL,
+  `checkpoint_order` int(11) NOT NULL,
   `checkpoint_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `checked_in_so_far` int(11) DEFAULT 0,
+  `checked_in_so_far` int(11) NOT NULL DEFAULT 0,
+  `first_checkin_at` datetime DEFAULT NULL,
+  `first_runner` char(5) DEFAULT NULL,
+  `most_recent_checkin_at` datetime DEFAULT NULL,
+  `most_recent_runner` char(5) DEFAULT NULL,
   PRIMARY KEY (`checkpoint_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-INSERT INTO `checkpoints` (`checkpoint_id`, `checkpoint_name`) VALUES
-(0, 'Registration'),
-(1, 'Checkpoint 1'),
-(2, 'Checkpoint 2'),
-(3, 'Checkpoint 3'),
-(4, 'Checkpoint 4'),
-(5, 'Checkpoint 5'),
-(6, 'Finish');
+INSERT INTO `checkpoints` (`checkpoint_id`, `checkpoint_order`, `checkpoint_name`) VALUES
+(substring(MD5(RAND()), -5), 0, 'Registration'),
+(substring(MD5(RAND()), -5), 1, 'Checkpoint 1'),
+(substring(MD5(RAND()), -5), 2, 'Checkpoint 2'),
+(substring(MD5(RAND()), -5), 3, 'Checkpoint 3'),
+(substring(MD5(RAND()), -5), 4, 'Checkpoint 4'),
+(substring(MD5(RAND()), -5), 5, 'Checkpoint 5'),
+(substring(MD5(RAND()), -5), 6, 'Finish');
+UPDATE checkpoints SET checkpoint_id = UPPER(checkpoint_id);
 
 -- --------------------------------------------------------
 
@@ -46,14 +51,15 @@ INSERT INTO `checkpoints` (`checkpoint_id`, `checkpoint_name`) VALUES
 
 DROP TABLE IF EXISTS `runners`;
 CREATE TABLE `runners` (
-  `runner_id` int(11) NOT NULL AUTO_INCREMENT,
-  `runner_code` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `runner_id` char(5) COLLATE utf8_unicode_ci NOT NULL,
   `player_email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `player_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `twitter_handle` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `is_tagged` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY `runner_id` (`runner_id`),
-  KEY `index_runners_on_code` (`runner_code`)
+  `player_twitter_handle` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `hashed_password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `is_tagged` tinyint(1) DEFAULT 0,
+  `num_tagged` int(11) DEFAULT 0,
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY `runner_id` (`runner_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -64,10 +70,9 @@ CREATE TABLE `runners` (
 
 DROP TABLE IF EXISTS `tags`;
 CREATE TABLE `tags` (
-  `tag_id` int(11) NOT NULL AUTO_INCREMENT,
-  `runner_id` int(11) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `tagger_id` int(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `runner_id` char(5) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `tagger_id` char(5) COLLATE utf8_unicode_ci DEFAULT NULL,
   `tag_time` datetime DEFAULT NULL,
-  PRIMARY KEY (`tag_id`),
-  UNIQUE KEY `runner_id` (`runner_id`)
+  PRIMARY KEY (`runner_id`),
+  KEY `index_tags_on_tagger` (`tagger_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ;
