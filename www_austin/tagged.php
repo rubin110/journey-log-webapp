2dee51a08eaba1bd65ec805f0b397cadda54c817
Thomas Lotze <thomas.lotze@thomaslotze.com>
2012-03-09 02:51:53 -0800

    avoiding double-counting tags when the target is already tagged

--- www_austin/tagged.php
+++ www_austin/tagged.php
@@ -5,8 +5,11 @@ $target_id = $command[1];
 # NOTE: $chaser_id may be empty
 $chaser_id = $command[2];
 
-tag($target_id, $chaser_id);
-
-echo "<p>Runner $target_id has been tagged.</p>";
+if (is_chaser($target_id)) {
+	echo "<p>Runner $target_id has been marked as tagged.</p>";
+} else {
+	tag($target_id, $chaser_id);
+	echo "<p>Runner $target_id has been tagged.</p>";
+}
 
 ?>
