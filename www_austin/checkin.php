68d967d79482196b1cf5cb0651fbe702b690d2f6
Thomas Lotze <thomas.lotze@thomaslotze.com>
2012-03-09 03:39:43 -0800

    checkin now checks to make sure you're not a chaser before checking you in

--- www_austin/checkin.php
+++ www_austin/checkin.php
@@ -13,18 +13,23 @@ if (!$cookie_rid) {
 	# TODO: this should never happen, unless someone tries entering a bad checkpoint code, so a confusing redirect/not mentioning the error is probably okay...but printing errors is usually better
 	redirect_to('/instructions');
 } else {
-	if (checkin($cookie_rid, $cid, $checked_in_so_far)) {
-		echo "<p>You successfully checked in at " , $checkpoint_name , ". Only " , $checked_in_so_far , " runners checked in here before you did.</p>";		
-		$seconds_since_start = time() - $game_start_time;
-		$m = (int)($seconds_since_start / 60); $s = $seconds_since_start % 60;
-		$h = (int)($m / 60); $m = $m % 60;
-		$timestring = str_pad($h,2,'0',STR_PAD_LEFT) . ":" . str_pad($m,2,'0',STR_PAD_LEFT) . ":" . str_pad($s,2,'0',STR_PAD_LEFT);
-		$twitterString="I survived to $checkpoint_name! Made it in $timestring, with only $checked_in_so_far runners ahead of me. #sxsw #jtteotn";
-		echo "<p>$twitterString</p>";
-		echo "<p>(TODO: link to tweet this: )</p>";
+	if (is_chaser($cookie_rid)) {
+		echo "<p>Sorry...you're a chaser.  We're glad you made it to this checkpoint, but your goal is now to tag runners.</p>";
 		echo "<p><a href='/runners/$cookie_rid'>Check your stats</a></p>";
 	} else {
-		echo "<p>You FAILED to check in at " , $checkpoint_name , "!</p>";				
+		if (checkin($cookie_rid, $cid, $checked_in_so_far)) {
+			echo "<p>You successfully checked in at " , $checkpoint_name , ". Only " , $checked_in_so_far , " runners checked in here before you did.</p>";		
+			$seconds_since_start = time() - $game_start_time;
+			$m = (int)($seconds_since_start / 60); $s = $seconds_since_start % 60;
+			$h = (int)($m / 60); $m = $m % 60;
+			$timestring = str_pad($h,2,'0',STR_PAD_LEFT) . ":" . str_pad($m,2,'0',STR_PAD_LEFT) . ":" . str_pad($s,2,'0',STR_PAD_LEFT);
+			$twitterString="I survived to $checkpoint_name! Made it in $timestring, with only $checked_in_so_far runners ahead of me. #sxsw #jtteotn";
+			echo "<p>$twitterString</p>";
+			echo "<p>(TODO: link to tweet this: )</p>";
+			echo "<p><a href='/runners/$cookie_rid'>Check your stats</a></p>";
+		} else {
+			echo "<p>You FAILED to check in at " , $checkpoint_name , "!</p>";				
+		}		
 	}
 }
 
